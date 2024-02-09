5cec9a1f7d509dbde3f5230491c23ad4324369c8
afshin <afshine1989@gmail.com>
2024-02-10 00:40:25 +0330

    Update ParticleSystem.java

--- LeonidsLib/src/main/java/com/plattysoft/leonids/ParticleSystem.java
+++ LeonidsLib/src/main/java/com/plattysoft/leonids/ParticleSystem.java
@@ -701,7 +701,9 @@ public class ParticleSystem {
 				}
 			}
 		}
-		mDrawingView.postInvalidate();
+		if(mDrawingView!=null){
+			mDrawingView.postInvalidate();
+		}
 	}
 
 	private void cleanupAnimation() {
